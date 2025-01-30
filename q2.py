d28105961e31ec7ab58076e6fb8558d4d88a642e
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-30 17:25:48 +0000

    Positive, Negative, Zero

---
+++ q2.py
@@ -0,0 +1,10 @@
+#Write a Program to find If the number is positive, Negative or Zero
+x=int(input("Enter your Number"))
+if x>0:
+    print("Your Number is Positive")
+elif x < 0:
+    print("Your Number is Negative")
+
+else:  
+    print("Your Number is Zero")
+
