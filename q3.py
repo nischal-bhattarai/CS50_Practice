b56e8f5067a065c436776837bb4dec33af8fe6ec
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-30 17:29:39 +0000

    Divisible by 5

---
+++ q3.py
@@ -0,0 +1,7 @@
+#Write a Program to check if the Number is Divisible by 5
+x=int(input("Eter your Number"))
+if x % 5 == 0:
+    print("Your Number is Divisible by 5")
+else:
+    print("Your Number is not Divisible by 5")
+    
