b7b10f21cdd8991d29dd00675d151f96f7a34d78
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-30 17:38:52 +0000

    Minimum of Two Number

---
+++ q5.py
@@ -0,0 +1,9 @@
+#To Find Minimum of Two Number
+x=int(input("Enter Your First Number"))
+y=int(input("Enter Your Second Number"))
+if x < y:
+    print("First Number is Smaller")
+elif x == y:
+    print("Both Number are Equal")
+else:
+    print("Second Number is Smaller")
