e067f2dd964b391603cad9e26838ecbfe111d9c3
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-30 17:35:55 +0000

    Maximum of Two Number

---
+++ q4.py
@@ -0,0 +1,10 @@
+#To Find Maximum of Two Number
+x=int(input("Enter Your First Number"))
+y=int(input("Enter Your Second Number"))
+if x > y:
+    print("First Number is Greater")
+elif x == y:
+    print("Both Number are Equal")
+else:
+    print("Second Number is Greater")
+    
