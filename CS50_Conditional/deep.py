81ebdec7c31bb142f55d3f4a4cea9a175304bec0
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-31 15:49:06 +0000

    Practice

---
+++ CS50_Conditional/deep.py
@@ -0,0 +1,7 @@
+user_input = input("What is the Answer to the Great Question of Lifr, the Universe, and Everything? ").lower()
+
+match user_input:
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
