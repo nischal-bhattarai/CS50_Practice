82b6982ba6923187672439f954d6431545a190b8
nischal-bhattarai <arsenalistnischal@gmail.com>
2025-01-31 13:43:41 +0000

    Week 1

---
+++ CS50_Conditional/week1.py
@@ -0,0 +1,10 @@
+x = int(input("Whats X ? "))
+y = int(input("Whats Y ? "))
+
+if x < y:
+    print("X is less than Y")
+if x > y:
+    print("X is greater than Y")
+if x == y:
+    print("X is Equal to Y")
+    
